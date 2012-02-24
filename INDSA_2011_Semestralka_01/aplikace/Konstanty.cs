a60122ed9b624051aa92d2387b5a024a951786b3
Matej Trakal <mtrakal@mtrakal.cz>
2012-02-24 02:47:06 +0100

    Přepsán html pro Google Api v3, přidání klikání polohy + města na poloze (případně PSČ)

--- INDSA_2011_Semestralka_01/aplikace/Konstanty.cs
+++ INDSA_2011_Semestralka_01/aplikace/Konstanty.cs
@@ -7,13 +7,8 @@ namespace aplikace {
     public static class Konstanty {
         public static string FORMAT = "{0};{1};{2};{3};{4};{5};{6}";
         public static string ABECEDA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        public static string FORMATVRCHOL = "var myLatLng = new google.maps.LatLng({1}, {2});\r\n" +
-            "var marker = new google.maps.Marker({{position: myLatLng, map: map, title:\"{0}\", icon:icon}});\r\n" +
-            "marker.setMap(map);\r\n";
-        public static string FORMATHRANA = "var routeCoords = [new google.maps.LatLng({0}, {1}), new google.maps.LatLng({2},{3})];\r\n" +
-            "var route = new google.maps.Polyline({{path: routeCoords, strokeColor: \"#0000ff\", strokeOpacity: 0.5, strokeWeight: 1.5}});\r\n" +
-            "route.setMap(map);\r\n" +
-            "var marker = new google.maps.Marker({{icon:\"pixel.gif\", position: new google.maps.LatLng({4}, {5}), draggable: false, map: map}});\r\n" +
-            "var label = new Label({{map: map}}); label.bindTo('position', marker, 'position'); label.set('text', '{6}');\r\n";
+        public static string FORMATVRCHOL = "markers.push(new google.maps.Marker({{position: new google.maps.LatLng({1}, {2}), map: map, title:\"{0}\", icon:icon}}));\r\n";
+        public static string FORMATHRANA = "new google.maps.Polyline({{path: [new google.maps.LatLng({0}, {1}), new google.maps.LatLng({2},{3})], strokeColor: \"#0000ff\", strokeOpacity: 0.5, strokeWeight: 1.5}}).setMap(map);\r\n"+
+        "var label = new Label({{map: map}}); label.bindTo('position', new google.maps.Marker({{icon:\"pixel.gif\", position: new google.maps.LatLng({4}, {5}), draggable: false, map: map}}), 'position'); label.set('text', '{6} km');\r\n";
     }
 }
